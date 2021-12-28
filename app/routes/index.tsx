80a54caf5a6c399349adb10207d8d28c0566ed60
Kyle Gill <kylerobertgill@gmail.com>
2021-12-27 19:25:05 -0700

    add basic loader

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,33 @@
+import { useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
+
+export let loader: LoaderFunction = () => {
+  return [{ name: "Pants" }, { name: "Jacket" }];
+};
+
 export default function Index() {
+  let data = useLoaderData();
+  console.log(data);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <section className="flex space-x-4 items-center width-100 sm:px-10 px-4 pt-10 pb-16 bg-gradient-to-br from-sky-400 to-indigo-500">
+        <div>Image</div>
+        <div>
+          <h1 className="text-3xl font-bold text-white mb-1">Goals</h1>
+          <div className="flex space-x-2 items-center text-white text-xs mb-1 italic ">
+            "...by small and simple things are great things brought to pass"
+          </div>
+          <div className="flex space-x-2 items-center">
+            Fingerprint
+            <p className="text-md text-white">Kyle Gill</p>
+          </div>
+          <div className="flex space-x-2 items-center">
+            Calendar
+            <p className="text-md text-white">2021</p>
+          </div>
+        </div>
+      </section>
       <h1 className="text-gray-600">Welcome to Remix</h1>
       <ul>
         <li>
